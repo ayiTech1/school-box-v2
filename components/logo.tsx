07b2f2b54eb693f81048ad705cd2b1e64bd91895
ayiTech1 <stephen.ayisah@stu.ucc.edu.gh>
2025-11-06 11:57:34 +0000

    update logo components

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import React from "react";
 
 const Logo = ({
   className,
@@ -10,17 +9,18 @@ const Logo = ({
   spanDesign?: string;
 }) => {
   return (
-    <Link href={"/"} className="inline-flex">
+    <Link href={"/"} className="inline-flex items-center">
       <h2
         className={cn(
-          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          // Responsive font sizes
+          "text-xl sm:text-2xl lg:text-3xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green transition-all duration-200 ease-in-out group font-sans",
           className
         )}
       >
         School
         <span
           className={cn(
-            "text-shop_light_green group-hover:text-shop_dark_green hoverEffect",
+            "text-shop_light_green group-hover:text-shop_dark_green transition-colors duration-200 ease-in-out",
             spanDesign
           )}
         >
